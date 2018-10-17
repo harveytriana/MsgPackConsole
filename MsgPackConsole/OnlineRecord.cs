6fc782192ec6c7f9c8f8beafbea05f15c0ff371a
Harvey Triana <harveytriana@hotmail.com>
2018-10-17 12:06:02 -0500

    Update 17-10-18
    Update 17-10-18

---
+++ MsgPackConsole/OnlineRecord.cs
@@ -0,0 +1,54 @@
+﻿// ===============================
+// ©Copyright VISIONARY SAS, 2018
+// ===============================
+using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// NOTE
+// For use BinaryFormatter is required a DLL, like this case.
+// For integrity
+
+namespace MML.Shared
+{
+    [MessagePackObject]
+    public class OnlineRecord
+    {
+        [Key(0)]
+        public string ID { get; set; }
+        [Key(1)]
+        public DateTime Time { get; set; }
+        [Key(2)]
+        public float Depth { get; set; }
+        [Key(3)]
+        public List<Parameter> Parameters { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Time.ToString("HH:mm:ss")}: {ID} {Depth.ToString("0.00")} | {Count()}";
+        }
+
+        public int Count()
+        {
+            return Parameters != null ? Parameters.Count : 0;
+        }
+
+        public float ValueOf(string mnemonic)
+        {
+            var y = Parameters?.FirstOrDefault(x => x.Mnemonic == mnemonic)?.Value;
+            return y == null ? -999.25F : y.Value;
+        }
+    }
+
+    [MessagePackObject]
+    public class Parameter
+    {
+        [Key(0)]
+        public string Mnemonic { get; set; }
+        [Key(1)]
+        public float Value { get; set; }
+
+        public override string ToString() => $"{Mnemonic}={Value};";
+    }
+}
---
+++ MsgPackConsole/Program.cs
@@ -0,0 +1,76 @@
+﻿using MessagePack;
+using MML.Shared;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// https://www.strathweb.com/2017/06/using-messagepack-with-asp-net-core-mvc/
+//  
+// https://github.com/WebApiContrib/WebAPIContrib.Core#formatters
+
+// Source
+// https://github.com/neuecc/MessagePack-CSharp/
+
+namespace MsgPackConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MessagePack Nuget");
+            Console.WriteLine("Author: Visionary S.A.S.\n");
+
+            var sample = GetOnlineRecord();
+            Console.WriteLine($"Object: {sample}\n");
+
+            var bytes = MessagePackSerializer.Serialize(sample);
+            Console.WriteLine($"Packed, bytes: {bytes.Length}");
+
+            var js = JsonConvert.SerializeObject(sample);
+            Console.WriteLine($"Pure JSON: {js.Length}");
+
+            var retrivedSample = MessagePackSerializer.Deserialize<OnlineRecord>(bytes);
+            Console.WriteLine($"Unpacked: {retrivedSample}");
+
+            var jsArray = JsonConvert.SerializeObject(bytes);
+            Console.WriteLine($"\nObject Packed to JSON array:\n{jsArray.Length}");
+
+            Console.Write("\n\nPause...");
+            Console.ReadKey();
+        }
+
+        private static OnlineRecord GetOnlineRecord()
+        {
+            var data = new OnlineRecord {
+                Time = DateTime.Now,
+                Depth = 2042.0F,
+                ID = "SAMP-0001",
+                Parameters = new List<Parameter>()
+            };
+
+            for (int i = 0; i < 260; i++) {
+                data.Parameters.Add(new Parameter { Mnemonic = RandomString(4), Value = (float)random.NextDouble() });
+            };
+
+            Console.WriteLine("OnlineRecord:");
+            Console.WriteLine("Time: {0}", data.Time);
+            Console.WriteLine("Depth: {0}", data.Depth);
+            data.Parameters.ForEach(p => {
+                Console.WriteLine("  {0}", p);
+            });
+
+            return data;
+        }
+
+        static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+
+
+}
